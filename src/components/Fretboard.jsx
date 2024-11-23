bb7321467e87bf124d563d6dc9031dbf0f55b468
Rob Gill <robgill@Robs-MacBook-Pro.local>
2024-11-23 12:43:33 -0500

    init react app

---
+++ src/components/Fretboard.jsx
@@ -0,0 +1,5 @@
+import fretboardImage from "./fretboard.png";
+
+export const Fretboard = () => {
+  return <img src={fretboardImage} alt="Fretboard" />;
+};
---
+++ src/utils/GetNotes.js
@@ -0,0 +1,15 @@
+export const getNotes = () => {
+  return [
+    "A",
+    "A#/Bb",
+    "B",
+    "C",
+    "C#/Db",
+    "D",
+    "D#/Eb",
+    "E",
+    "F",
+    "F#/Gb",
+    "G",
+  ];
+};
